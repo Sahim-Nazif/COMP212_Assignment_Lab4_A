7b1b737b1bc3856a48ff518564b66512001c785d
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-02-12 14:09:43 -0500

    updated lable

--- Base_Form_Assignment4/Form1.Designer.cs
+++ Base_Form_Assignment4/Form1.Designer.cs
@@ -48,45 +48,50 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(257, 18);
+            this.label1.Location = new System.Drawing.Point(386, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(270, 24);
+            this.label1.Size = new System.Drawing.Size(399, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome To Career Choice";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(112, 104);
+            this.label2.Location = new System.Drawing.Point(168, 160);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.Size = new System.Drawing.Size(86, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "First Name";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(112, 131);
+            this.label3.Location = new System.Drawing.Point(168, 202);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.Size = new System.Drawing.Size(86, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Last Name";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(112, 163);
+            this.label4.Location = new System.Drawing.Point(168, 251);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.Size = new System.Drawing.Size(48, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Email";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(112, 195);
+            this.label5.Location = new System.Drawing.Point(168, 300);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.Size = new System.Drawing.Size(68, 20);
             this.label5.TabIndex = 4;
             this.label5.Text = "Phone #";
             // 
@@ -95,55 +100,62 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.DarkGreen;
-            this.label6.Location = new System.Drawing.Point(111, 73);
+            this.label6.Location = new System.Drawing.Point(166, 112);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(319, 16);
+            this.label6.Size = new System.Drawing.Size(530, 25);
             this.label6.TabIndex = 5;
-            this.label6.Text = "Enter Your Details To Be Contacted for Opportunies :";
+            this.label6.Text = "Enter Your Details To be Contacted for IT Job Opportunies :";
             // 
             // txtFName
             // 
-            this.txtFName.Location = new System.Drawing.Point(194, 101);
+            this.txtFName.Location = new System.Drawing.Point(291, 155);
+            this.txtFName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtFName.Name = "txtFName";
-            this.txtFName.Size = new System.Drawing.Size(236, 20);
+            this.txtFName.Size = new System.Drawing.Size(352, 26);
             this.txtFName.TabIndex = 6;
             // 
             // txtLName
             // 
-            this.txtLName.Location = new System.Drawing.Point(194, 131);
+            this.txtLName.Location = new System.Drawing.Point(291, 202);
+            this.txtLName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtLName.Name = "txtLName";
-            this.txtLName.Size = new System.Drawing.Size(236, 20);
+            this.txtLName.Size = new System.Drawing.Size(352, 26);
             this.txtLName.TabIndex = 7;
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(194, 163);
+            this.txtEmail.Location = new System.Drawing.Point(291, 251);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(236, 20);
+            this.txtEmail.Size = new System.Drawing.Size(352, 26);
             this.txtEmail.TabIndex = 8;
             // 
             // txtPhone
             // 
-            this.txtPhone.Location = new System.Drawing.Point(194, 195);
+            this.txtPhone.Location = new System.Drawing.Point(291, 300);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtPhone.Name = "txtPhone";
-            this.txtPhone.Size = new System.Drawing.Size(236, 20);
+            this.txtPhone.Size = new System.Drawing.Size(352, 26);
             this.txtPhone.TabIndex = 9;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(112, 257);
+            this.label7.Location = new System.Drawing.Point(168, 395);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(45, 13);
+            this.label7.Size = new System.Drawing.Size(68, 20);
             this.label7.TabIndex = 10;
             this.label7.Text = "Address";
             // 
             // txtAddress
             // 
-            this.txtAddress.Location = new System.Drawing.Point(194, 233);
+            this.txtAddress.Location = new System.Drawing.Point(291, 358);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtAddress.Multiline = true;
             this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(236, 55);
+            this.txtAddress.Size = new System.Drawing.Size(352, 82);
             this.txtAddress.TabIndex = 11;
             // 
             // btnSubmit
@@ -151,9 +163,10 @@
             this.btnSubmit.BackColor = System.Drawing.SystemColors.Highlight;
             this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSubmit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnSubmit.Location = new System.Drawing.Point(194, 312);
+            this.btnSubmit.Location = new System.Drawing.Point(291, 480);
+            this.btnSubmit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(117, 36);
+            this.btnSubmit.Size = new System.Drawing.Size(176, 55);
             this.btnSubmit.TabIndex = 12;
             this.btnSubmit.Text = "Submit Now";
             this.btnSubmit.UseVisualStyleBackColor = false;
@@ -162,16 +175,18 @@
             // listBox
             // 
             this.listBox.FormattingEnabled = true;
-            this.listBox.Location = new System.Drawing.Point(455, 101);
+            this.listBox.ItemHeight = 20;
+            this.listBox.Location = new System.Drawing.Point(682, 155);
+            this.listBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(226, 134);
+            this.listBox.Size = new System.Drawing.Size(337, 204);
             this.listBox.TabIndex = 14;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.listBox);
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.txtAddress);
@@ -186,6 +201,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
